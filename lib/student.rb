829a017a67a01a6117ea85fe4233654b95cc193e
Nadav Schwartz <nadavschwartz58@gmail.com>
2020-07-17 17:44:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,23 +27,40 @@ attr_reader :id
   end
   
   def save
+    if self.id
+      self.update
+    else
     sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
-
-    DB[:conn].execute(sql, self.name, self.grade)
-  end
  
-  def self.create(name:, grade:)
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+  end
+ end
+ 
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
     student
   end
  
   def self.find_by_name(name)
-    sql = "SELECT * FROM songs WHERE name = ?"
+    sql = "SELECT * FROM students WHERE name = ?"
     result = DB[:conn].execute(sql, name)[0]
     Student.new(result[0], result[1], result[2])
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end  
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
 end
